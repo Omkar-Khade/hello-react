ed1b6d89b2ab2879e6ac40b27f6b21d74bc825bc
Khade <Omkar.Khade@ap.jll.com>
2022-11-18 11:37:32 +0530

    Created Navbar and Home

---
+++ src/component/Navbar.js
@@ -0,0 +1,13 @@
+import react from "react";
+import "./Navbar.css"
+
+function Navbar() {
+    return (
+      <div className="Nav">
+        <span className="NavMenu"> In Progress </span>
+        <span className="NavMenu"> Completed </span>
+      </div>
+    );
+  }
+  
+  export default Navbar;
